c30283f9285f79861a7250a3fe030e3b187c951f
Martin <you@example.com>
2019-11-23 14:00:01 +0100

    uppdatering

--- dice.py
+++ dice.py
@@ -1,8 +1,17 @@
 import random
+import time
 
+print(""" ┬ ┬┌─┐┬  ┌─┐┌─┐┌┬┐┌─┐  ┌┬┐┌─┐  ╔╦╗┌─┐┬─┐┌┬┐┬┌┐┌┌─┐  ┌─┐┌─┐┌┬┐┌─┐┬
+ │││├┤ │  │  │ ││││├┤    │ │ │  ║║║├─┤├┬┘ │ ││││└─┐  │ ┬├─┤│││├┤ │
+ └┴┘└─┘┴─┘└─┘└─┘┴ ┴└─┘   ┴ └─┘  ╩ ╩┴ ┴┴└─ ┴ ┴┘└┘└─┘  └─┘┴ ┴┴ ┴└─┘o""")
+                                                                                                             
 rolled_num = random.randint(1,6)
-print("Du fick: ", rolled_num)
-input("Vill du slå igen?")
+time.sleep(2)
+print("Du fick: ", rolled_num) 
+time.sleep(2)
+input("Vill du slå igen?") 
+
+
         
       
 
